ba4d20f09735b47d0a1dcc7e06cf3016aca10392
GeorgeSeif <george.seif94@gmail.com>
2017-09-24 12:58:01 -0400

    Added plotting helpers

---
+++ plt_helpers.py
@@ -0,0 +1,164 @@
+import matplotlib
+ 
+def scatterplot(x_data, y_data, x_label, y_label, title):
+
+    # Create the plot object
+    _, ax = plt.subplots()
+
+    # Plot the data, set the size (s), color and transparency (alpha)
+    # of the points
+    ax.scatter(x_data, y_data, s = 30, color = '#539caf', alpha = 0.75)
+
+    # Label the axes and provide a title
+    ax.set_title(title)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+
+
+def lineplot(x_data, y_data, x_label, y_label, title):
+    # Create the plot object
+    _, ax = plt.subplots()
+
+    # Plot the best fit line, set the linewidth (lw), color and
+    # transparency (alpha) of the line
+    ax.plot(x_data, y_data, lw = 2, color = '#539caf', alpha = 1)
+
+    # Label the axes and provide a title
+    ax.set_title(title)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+
+
+# Line plot with 2 different y values
+def lineplot2y(x_data, x_label, y1_data, y1_color, y1_label, y2_data, y2_color, y2_label, title):
+    # Each variable will actually have its own plot object but they
+    # will be displayed in just one plot
+    # Create the first plot object and draw the line
+    _, ax1 = plt.subplots()
+    ax1.plot(x_data, y1_data, color = y1_color)
+    # Label axes
+    ax1.set_ylabel(y1_label, color = y1_color)
+    ax1.set_xlabel(x_label)
+    ax1.set_title(title)
+
+    # Create the second plot object, telling matplotlib that the two
+    # objects have the same x-axis
+    ax2 = ax1.twinx()
+    ax2.plot(x_data, y2_data, color = y2_color)
+    ax2.set_ylabel(y2_label, color = y2_color)
+    # Show right frame line
+    ax2.spines['right'].set_visible(True)
+
+
+def histogram(data, x_label, y_label, title):
+    _, ax = plt.subplots()
+    ax.hist(data, color = '#539caf')
+    ax.set_ylabel(y_label)
+    ax.set_xlabel(x_label)
+    ax.set_title(title)
+
+
+
+# Overlay 2 histograms to compare them
+def overlaid_histogram(data1, data1_name, data1_color, data2, data2_name, data2_color, x_label, y_label, title):
+    # Set the bounds for the bins so that the two distributions are
+    # fairly compared
+    max_nbins = 10
+    data_range = [min(min(data1), min(data2)), max(max(data1), max(data2))]
+    binwidth = (data_range[1] - data_range[0]) / max_nbins
+    bins = np.arange(data_range[0], data_range[1] + binwidth, binwidth)
+
+    # Create the plot
+    _, ax = plt.subplots()
+    ax.hist(data1, bins = bins, color = data1_color, alpha = 1, label = data1_name)
+    ax.hist(data2, bins = bins, color = data2_color, alpha = 0.75, label = data2_name)
+    ax.set_ylabel(y_label)
+    ax.set_xlabel(x_label)
+    ax.set_title(title)
+    ax.legend(loc = 'best')
+
+
+# Probability Density Function
+def densityplot(x_data, density_est, x_label, y_label, title):
+    _, ax = plt.subplots()
+    ax.plot(x_data, density_est(x_data), color = '#539caf', lw = 2)
+    ax.set_ylabel(y_label)
+    ax.set_xlabel(x_label)
+    ax.set_title(title)
+
+
+
+def barplot(x_data, y_data, error_data, x_label, y_label, title):
+    _, ax = plt.subplots()
+    # Draw bars, position them in the center of the tick mark on the x-axis
+    ax.bar(x_data, y_data, color = '#539caf', align = 'center')
+    # Draw error bars to show standard deviation, set ls to 'none'
+    # to remove line between points
+    ax.errorbar(x_data, y_data, yerr = error_data, color = '#297083', ls = 'none', lw = 2, capthick = 2)
+    ax.set_ylabel(y_label)
+    ax.set_xlabel(x_label)
+    ax.set_title(title)
+
+
+
+def stackedbarplot(x_data, y_data_list, y_data_names, colors, x_label, y_label, title):
+    _, ax = plt.subplots()
+    # Draw bars, one category at a time
+    for i in range(0, len(y_data_list)):
+        if i == 0:
+            ax.bar(x_data, y_data_list[i], color = colors[i], align = 'center', label = y_data_names[i])
+        else:
+            # For each category after the first, the bottom of the
+            # bar will be the top of the last category
+            ax.bar(x_data, y_data_list[i], color = colors[i], bottom = y_data_list[i - 1], align = 'center', label = y_data_names[i])
+    ax.set_ylabel(y_label)
+    ax.set_xlabel(x_label)
+    ax.set_title(title)
+    ax.legend(loc = 'upper right')
+
+
+
+def groupedbarplot(x_data, y_data_list, y_data_names, colors, x_label, y_label, title):
+    _, ax = plt.subplots()
+    # Total width for all bars at one x location
+    total_width = 0.8
+    # Width of each individual bar
+    ind_width = total_width / len(y_data_list)
+    # This centers each cluster of bars about the x tick mark
+    alteration = np.arange(-(total_width/2), total_width/2, ind_width)
+
+    # Draw bars, one category at a time
+    for i in range(0, len(y_data_list)):
+        # Move the bar to the right on the x-axis so it doesn't
+        # overlap with previously drawn ones
+        ax.bar(x_data + alteration[i], y_data_list[i], color = colors[i], label = y_data_names[i], width = ind_width)
+    ax.set_ylabel(y_label)
+    ax.set_xlabel(x_label)
+    ax.set_title(title)
+    ax.legend(loc = 'upper right')
+
+
+
+
+def boxplot(x_data, y_data, base_color, median_color, x_label, y_label, title):
+    _, ax = plt.subplots()
+
+    # Draw boxplots, specifying desired style
+    ax.boxplot(y_data
+               # patch_artist must be True to control box fill
+               , patch_artist = True
+               # Properties of median line
+               , medianprops = {'color': median_color}
+               # Properties of box
+               , boxprops = {'color': base_color, 'facecolor': base_color}
+               # Properties of whiskers
+               , whiskerprops = {'color': base_color}
+               # Properties of whisker caps
+               , capprops = {'color': base_color})
+
+    # By default, the tick label starts at 1 and increments by 1 for
+    # each box drawn. This sets the labels to the ones we want
+    ax.set_xticklabels(x_data)
+    ax.set_ylabel(y_label)
+    ax.set_xlabel(x_label)
+    ax.set_title(title)
